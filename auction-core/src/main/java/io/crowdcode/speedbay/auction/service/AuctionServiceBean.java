1ad20c57b3e913053ef126302f7e52c257a97ad9
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-09-13 15:08:43 +0200

    Solution 4

--- auction-core/src/main/java/io/crowdcode/speedbay/auction/service/AuctionServiceBean.java
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/service/AuctionServiceBean.java
@@ -1,7 +1,18 @@
 package io.crowdcode.speedbay.auction.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static io.crowdcode.speedbay.common.AnsiColor.green;
+
 /**
  * @author Ingo Düppe (Crowdcode)
  */
 public class AuctionServiceBean implements AuctionService {
+
+    private static final Logger log = LoggerFactory.getLogger(AuctionServiceBean.class);
+
+    public AuctionServiceBean() {
+        log.info(green("Here I am!"));
+    }
 }
