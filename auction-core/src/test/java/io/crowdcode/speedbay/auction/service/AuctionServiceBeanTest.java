ae04271523795d9a617fd7be652e4a881b0d7548
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-09-13 15:00:28 +0200

    Solution 3

--- auction-core/src/test/java/io/crowdcode/speedbay/auction/service/AuctionServiceBeanTest.java
+++ auction-core/src/test/java/io/crowdcode/speedbay/auction/service/AuctionServiceBeanTest.java
@@ -1,7 +1,10 @@
 package io.crowdcode.speedbay.auction.service;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import java.util.Map;
 
 import static org.junit.Assert.assertNotNull;
 
@@ -15,8 +18,21 @@ public class AuctionServiceBeanTest {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
 
 
-        AuctionService auctionService = context.getBean("auctionService", AuctionService.class);
+
+        AuctionService auctionService = context.getBean("auctionServiceBean", AuctionService.class);
+        AuctionService byName = (AuctionService) context.getBean("auctionServiceBean");
+        AuctionService byType = context.getBean(AuctionService.class);
+
+        Class<?> auctionServiceBean = context.getType("auctionServiceBean");
+        Assert.assertEquals(AuctionServiceBean.class, auctionServiceBean);
+
+        Map<String, AuctionService> beansOfType = context.getBeansOfType(AuctionService.class);
+        beansOfType.forEach((k,v)->System.out.print(k + " "+v.toString()));
+
+
         assertNotNull(auctionService);
+
+        context.close();
     }
 
 }
