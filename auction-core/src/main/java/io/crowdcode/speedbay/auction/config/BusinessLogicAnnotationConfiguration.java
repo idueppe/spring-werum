69aa6425376385b919d55434587a92c877e7d276
Ingo Düppe <ingo.dueppe@crowdcode.io>
2016-09-14 11:24:50 +0200

    Solution 9

---
+++ auction-core/src/main/java/io/crowdcode/speedbay/auction/config/BusinessLogicAnnotationConfiguration.java
@@ -0,0 +1,24 @@
+package io.crowdcode.speedbay.auction.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+/**
+ * @author Ingo Düppe (Crowdcode)
+ */
+@Configuration
+@ComponentScan(
+        basePackages = {
+                "io.crowdcode.speedbay.auction",
+                "io.crowdcode.speedbay.common.inmemory"},
+        excludeFilters =
+                {
+                        @ComponentScan.Filter(type = FilterType.REGEX, pattern = "io\\.crowdcode\\.speedbay\\.auction\\.config.*"),
+                        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*ApplicationLog.*")
+                }
+)
+public class BusinessLogicAnnotationConfiguration {
+
+
+}
